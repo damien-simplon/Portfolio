5895eeac3f777f513a44cd5b390c2ff8086c4b89
damien-simplon <damien.bellec12@gmail.com>
2020-11-15 16:46:16 +0100

    remplissage du head

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-echo "hello";
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Damien Bellec">
+    <meta name="description" content="Portfolio de Damien Bellec">
+    <meta name="keywords" content="Damien Bellec, Portfolio, Développeur web">
+    <title>Damien Bellec | Portfolio</title>
+    <link rel="icon" type="image/png" href="images/favicon." />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+    
+</body>
+</html>
