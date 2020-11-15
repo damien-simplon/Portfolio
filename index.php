f4c554262d7e5064382d53a84b7402fab962f867
damien-simplon <damien.bellec12@gmail.com>
2020-11-15 19:00:43 +0100

    création du logo, changement du favicon, commencement du header

--- index.php
+++ index.php
@@ -7,11 +7,16 @@
     <meta name="description" content="Portfolio de Damien Bellec">
     <meta name="keywords" content="Damien Bellec, Portfolio, Développeur web">
     <title>Damien Bellec | Portfolio</title>
-    <link rel="icon" type="image/png" href="images/favicon.png" />
+    <link rel="icon" type="image/png" href="images/logo.png" />
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body>
-    
+    <div id="header" class="section">
+        <div id="headerBlock">
+            <img src="images/logo.png" alt="">
+        </div>
+    </div>
 </body>
 </html>
