07b95a19157fe7bbd28a8e9ef49991414cc7a7fe
damien-simplon <damien.bellec12@gmail.com>
2020-11-17 14:30:34 +0100

    navbar terminée, commencement de la tête de page

--- index.php
+++ index.php
@@ -7,15 +7,26 @@
     <meta name="description" content="Portfolio de Damien Bellec">
     <meta name="keywords" content="Damien Bellec, Portfolio, Développeur web">
     <title>Damien Bellec | Portfolio</title>
-    <link rel="icon" type="image/png" href="images/logo.png" />
+    <link rel="icon" type="image/png" href="images/logo_borderless.png" />
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body>
     <div id="header" class="section">
+        <nav>
+            <ul>
+                <li><a href="#header">Accueil</a></li>
+                <li><a href="#apropos">À propos</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
         <div id="headerBlock">
-            <img src="images/logo.png" alt="">
+            <img src="images/logo_bordered.png" alt="">
+            <div id="headerBlockText">    
+                <p>Damien Bellec<span>Développeur Web</span></p>
+            </div>
         </div>
     </div>
 </body>
